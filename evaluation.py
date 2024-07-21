61188cb06c1ed76ba35dd0eb7e72486849b2f173
tomhruby1 <tom.hruby1@gmail.com>
2024-07-21 16:30:23 +0200

    fixed the fuzzyPP metric

--- evaluation.py
+++ evaluation.py
@@ -16,34 +16,44 @@ def fuzzy_PP(clusters, landmarks, t=1, t_max=3, planar=True):
     matches = {l:None for l in landmarks}
     unnassigned_clusters = copy.deepcopy(clusters)
     PP_val = 0.0
-
+    FN_val = 0
     for l_id, landmark in landmarks.items():
+        
+        candidate_clusters = [c for c in unnassigned_clusters if c.category == landmark['class']]
         if planar:
-            cluster_dists = [np.linalg.norm(landmark['projected_coord'][:2] - c.centroid[:2])
-                             for c in unnassigned_clusters if c.category == landmark['class']]
+            cluster_dists = [np.linalg.norm(landmark['projected_coord'][:2] - c.centroid[:2]) for c in candidate_clusters]
         else:
-            cluster_dists = [np.linalg.norm(landmark['projected_coord'] - c.centroid)
-                             for c in unnassigned_clusters if c.category == landmark['class']]
+            cluster_dists = [np.linalg.norm(landmark['projected_coord'] - c.centroid) for c in candidate_clusters]
           
         if len(cluster_dists) > 0:
             cl_idx = np.argmin(cluster_dists)
-            selected_cluser = unnassigned_clusters[cl_idx]
+            selected_cluster = candidate_clusters[cl_idx]
             if cluster_dists[cl_idx] < t_max:
-                # check that no other clusters with the same category are within the threshold
-                of_same_class_within = [np.linalg.norm(landmark['projected_coord'] - c.centroid) < t_max 
-                                        for c in clusters if c.category == landmark['class']]
+                # check that no other clusters from all the clawith the same category are within the threshold
+                                            # vvvv this compute dumb
+                if planar:
+                    of_same_class_within = [np.linalg.norm(landmark['projected_coord'][:2] - c.centroid[:2]) < t_max 
+                                            for c in clusters if c.category == landmark['class']]
+                else:
+                    of_same_class_within = [np.linalg.norm(landmark['projected_coord'] - c.centroid) < t_max 
+                                            for c in clusters if c.category == landmark['class']]
+                    
                 if sum(of_same_class_within) == 1:
-                    matches[l_id] = selected_cluser.id
-                    unnassigned_clusters.pop(cl_idx)
+                    matches[l_id] = selected_cluster.id
+                    unnasigned_idx = [i for i, c in enumerate(unnassigned_clusters) if c.id == selected_cluster.id][0]
+                    unnassigned_clusters.pop(unnasigned_idx)
                     d = cluster_dists[cl_idx]
                     pp = 1.0 if cluster_dists[cl_idx] < t else -1.0*d / (t_max - t) + t_max / (t_max - t)
                     PP_val += pp
                     print(f"PP for landmark {l_id}: {pp}")
+                else:
+                    print(f"multiple clusters of the same class within threshold for landmark {l_id}")
         
         if matches[l_id] is None:
             print(f"no match for landmark {l_id}")
+            FN_val += 1
 
-    return PP_val, matches
+    return PP_val, FN_val, matches
 
 
 def test():
@@ -70,10 +80,6 @@ def test():
     TP, matches = fuzzy_PP(clusters, landmarks, t=1, t_max=3, planar=True)
     print(f"TP: {TP}")
     print(f"matches: {matches}")
-
-    # assert TP == 6.0
-    # assert all([matches[l] is not None for l in landmarks])
-
 
 if __name__=='__main__':
     test()
